4f3e568cd373154e942d6e223b3158be3a00eb86
Anisberto Reis <anisbertoos@gmail.com>
2021-01-08 21:09:19 -0300

    Add home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Home from './Home';
 import NavBar from './Navbar';
 function App() {
   return (
-    <div>
+    <>
       <NavBar />
-    </div>
+      <Home />
+    </>
   );
 }
 
